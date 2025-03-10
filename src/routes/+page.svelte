cfe1600ee2f3ba7bd70fba744b8d89dea1e35734
lu7ue <zhan0067@hz.nl>
2025-03-10 14:00:56 +0100

    create a button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1 class="text-test text-huge">Hello, world!</h1>
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="container mx-auto text-center">
+	<h1 class="text-test text-huge">Tailwind vs. CSS</h1>
+	<h2 class="text-2xl text-gray-800">
+		Example 1 -> 
+		<a
+			class="underline"
+			href="/button"
+			on:click={(e) => {
+				e.preventDefault();
+				goto('/button');
+			}}
+		>
+			'Button'
+		</a>
+	</h2>
+</div>
---
+++ src/routes/button/+page.svelte
@@ -0,0 +1,27 @@
+<div class="container mx-auto flex flex-col items-center">
+	<button class="mt-10 block rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+		>Tailwind Button</button
+	>
+	<button class="button-settings hover-transition">CSS Button</button>
+</div>
+
+<style>
+	.button-settings {
+		margin-top: 40px; /* equals to mt-10 */
+		display: block; /* equals to block */
+		background-color: #3b82f6; /* equals to bg-blue-500 */
+		padding: 8px 16px; /* equals to px-4 and py-2 */
+        color: white; /* equals to text-white */
+		border-radius: 0.25rem; /* make a rounded corner */
+		font-size: 1rem; /* set the same default font size with Tailwind */
+		text-align: center; /* make the text center */
+	}
+	.hover-transition {
+		transition: background-color 0.2s ease-in-out;
+	}
+	@media (hover: hover) {
+		.hover-transition:hover {
+			background-color: #1d4ed8;
+		}
+	}
+</style>
